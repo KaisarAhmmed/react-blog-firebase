d349740a4d0a5ed174ae872b54db435285fa107d
Kaisar Ahmmed <kaisar123k@gmail.com>
2022-04-09 17:14:04 +0600

    set up

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="text-center text-sm">
+            <p>© 2022 Qurno. All rights reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
